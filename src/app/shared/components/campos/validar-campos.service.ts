d0b071c41db2648bb2a86342bf10e1dcac2590c6
Gabriela Vespoli <gabiat3@hotmail.com>
2021-09-12 13:40:41 -0300

    feat: update validators service

--- src/app/shared/components/campos/validar-campos.service.ts
+++ src/app/shared/components/campos/validar-campos.service.ts
@@ -8,6 +8,13 @@ export class ValidarCamposService {
 
   constructor() { }
 
+  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+    if ((control.touched || control.dirty) && this.hasError(control, errorName)) {
+      return true;
+    }
+    return false;
+  }
+
   hasError(control: AbstractControl, errorName: string): boolean {
     return control.hasError(errorName);
   }
