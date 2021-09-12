9b6e20ae86ebc7d46aa4997b1baad60d4da1cc9b
Gabriela Vespoli <gabiat3@hotmail.com>
2021-09-12 12:44:28 -0300

    feat: creating validators service

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidarCamposService } from "./../../shared/components/campos/validar-campos.service";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'dio-cadastro-filmes',
-  templateUrl: './cadastro-filmes.component.html',
-  styleUrls: ['./cadastro-filmes.component.scss']
+  selector: "dio-cadastro-filmes",
+  templateUrl: "./cadastro-filmes.component.html",
+  styleUrls: ["./cadastro-filmes.component.scss"],
 })
 export class CadastroFilmesComponent implements OnInit {
-
   cadastro: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    public validacao: ValidarCamposService,
+    private fb: FormBuilder
+  ) {}
 
   get f() {
     return this.cadastro.controls;
   }
 
   ngOnInit() {
-
     this.cadastro = this.fb.group({
-      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
-      urlFoto: ['', [Validators.minLength(10)]],
-      dtLancamento: ['', [Validators.required]],
-      descricao: [''],
+      titulo: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
+        ],
+      ],
+      urlFoto: ["", [Validators.minLength(10)]],
+      dtLancamento: ["", [Validators.required]],
+      descricao: [""],
       nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
-      urlIMBd: ['', [Validators.minLength(10)]],
-      genero: ['', [Validators.required]]
+      urlIMBd: ["", [Validators.minLength(10)]],
+      genero: ["", [Validators.required]],
     });
-
   }
 
   salvar(): void {
@@ -36,11 +44,10 @@ export class CadastroFilmesComponent implements OnInit {
       return;
     }
 
-    alert('SUCESSO\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+    alert("SUCESSO\n\n" + JSON.stringify(this.cadastro.value, null, 4));
   }
 
   reiniciarForm(): void {
     this.cadastro.reset();
   }
-
 }
