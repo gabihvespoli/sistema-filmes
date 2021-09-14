c47329e51fe6d59c120aba4fffd828cb1dcc4e46
Gabriela Vespoli <gabiat3@hotmail.com>
2021-09-14 00:08:21 -0300

    feat: componentizing input-number

--- src/app/shared/components/campos/input-number/input-number.component.ts
+++ src/app/shared/components/campos/input-number/input-number.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+import { ValidarCamposService } from '../validar-campos.service';
 
 @Component({
-  selector: 'input-number',
+  selector: 'dio-input-number',
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.scss']
 })
-export class InputNumberComponent implements OnInit {
+export class InputNumberComponent{
 
-  constructor() { }
+  @Input() titulo: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+  @Input() min = 0;
+  @Input() max = 10;
+  @Input() step = 1;
 
-  ngOnInit() {
+  constructor(public validacao: ValidarCamposService) { }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
 }
