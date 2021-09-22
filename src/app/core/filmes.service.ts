17a79af66733c5cc966e112a88a9f737c42bba20
Gabriela Vespoli <gabiat3@hotmail.com>
2021-09-21 21:31:05 -0300

    feat: add infinite scroll

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
@@ -16,7 +16,10 @@ export class FilmesService {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(): Observable<Filme[]> {
-    return this.http.get<Filme[]>(url);
+  listar(pagina:number, qtdPags: number): Observable<Filme[]> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.set('_page', pagina.toString())
+    httpParams = httpParams.set('_limit', qtdPags.toString());
+    return this.http.get<Filme[]>(url, {params: httpParams});
   }
 }
--- src/app/filmes/filmes.module.ts
+++ src/app/filmes/filmes.module.ts
@@ -1,4 +1,3 @@
-import { CamposModule } from './../shared/components/campos/campos.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -6,6 +5,8 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.component';
+import { CamposModule } from './../shared/components/campos/campos.module';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.compo
     MaterialModule,
     ReactiveFormsModule,
     FormsModule,
-    CamposModule
+    CamposModule,
+    InfiniteScrollModule
   ],
   declarations: [CadastroFilmesComponent, ListagemFilmesComponent]
 })
