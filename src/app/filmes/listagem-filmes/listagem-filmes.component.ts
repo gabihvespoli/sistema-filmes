67f345cda021a7711e6f242f1439f6833fb6d8ba
Gabriela Vespoli <gabiat3@hotmail.com>
2021-09-25 17:00:12 -0300

    feat: filter form

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,6 +1,8 @@
+import { FormBuilder } from '@angular/forms';
 import { FilmesService } from './../../core/filmes.service';
 import { Component, OnInit } from '@angular/core';
 import { Filme } from 'src/app/shared/models/filme';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -12,10 +14,30 @@ export class ListagemFilmesComponent implements OnInit {
   readonly qtdPags = 4;
   pagina = 0;
   filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(
+    private filmesService: FilmesService,
+    private fb: FormBuilder
+    ) { }
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.generos = [
+      "Ação",
+      "Aventura",
+      "Ficção Científica",
+      "Romance",
+      "Terror",
+      "Drama",
+      "Comédia",
+    ]
+
     this.listarFilmes();
   }
 
