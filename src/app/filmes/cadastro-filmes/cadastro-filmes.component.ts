7dfe3dbcda06b5a42f90e07ff63d0904a5c0e72d
Gabriela Vespoli <gabiat3@hotmail.com>
2021-09-28 22:27:47 -0300

    feat: editing movies

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { MatDialog } from "@angular/material/dialog";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Alerta } from './../../shared/models/alerta';
@@ -16,36 +16,31 @@ import { ValidarCamposService } from "./../../shared/components/campos/validar-c
 export class CadastroFilmesComponent implements OnInit {
   cadastro: FormGroup;
   generos: Array<string>;
+  id: number;
 
   constructor(
     public validacao: ValidarCamposService,
     public dialog: MatDialog,
     private fb: FormBuilder,
     private filmeService: FilmesService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
   ) {}
 
   get f() {
     return this.cadastro.controls;
   }
 
-  ngOnInit() {
-    this.cadastro = this.fb.group({
-      titulo: [
-        "",
-        [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(256),
-        ],
-      ],
-      urlFoto: ["", [Validators.minLength(10)]],
-      dtLancamento: ["", [Validators.required]],
-      descricao: [""],
-      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
-      urlIMBd: ["", [Validators.minLength(10)]],
-      genero: ["", [Validators.required]],
-    });
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+
+    if(this.id) {
+      this.filmeService.visualizar(this.id).subscribe((filme: Filme) => {
+        this.criarFormulario(filme);
+      });
+    } else {
+      this.criarFormulario(this.criarFilmeEmBranco());
+    }
 
     this.generos = [
       "Ação",
@@ -71,6 +66,38 @@ export class CadastroFilmesComponent implements OnInit {
 
   reiniciarForm(): void {
     this.cadastro.reset();
+  }
+
+  private criarFormulario(filme: Filme): void {
+    this.cadastro = this.fb.group({
+      titulo: [
+        filme.titulo,
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(256),
+        ],
+      ],
+      urlFoto: [filme.urlFoto, [Validators.minLength(10)]],
+      dtLancamento: [filme.dtLancamento, [Validators.required]],
+      descricao: [filme.descricao],
+      nota: [filme.nota, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMBd: [filme.urlIMDb, [Validators.minLength(10)]],
+      genero: [filme.genero, [Validators.required]],
+    });
+  }
+
+  private criarFilmeEmBranco(): Filme {
+    return {
+      id: null,
+      titulo: null,
+      urlFoto: null,
+      dtLancamento: null,
+      descricao: null,
+      nota: null,
+      urlIMBd: null,
+      genero: null,
+    } as Filme;
   }
 
   private salvar(filme: Filme): void {
@@ -106,4 +133,5 @@ export class CadastroFilmesComponent implements OnInit {
       }
     );
   }
+
 }
